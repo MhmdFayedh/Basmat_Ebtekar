ede845b05f6277a784dd855c1c5aab56e8bc07ab
Mohammed <mohammed@mofayedh.com>
2022-04-13 05:52:29 +0300

    Commit the full project

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+let toggler = document.querySelector(".toggle");
+let close_btn = document.querySelector(".close");
+let nav = document.querySelector("._navbar");
+let lowerNav = document.querySelector(".lower-nav");
+let scrollUp = document.querySelector('.scroll-up');
+let changeLang = document.querySelector(".change-lang");
+let navItems = document.querySelectorAll(".nav-item");
+
+// let toggleColor = document.querySelectorAll(".line");
+
+
+
+toggler.onclick = function () {
+    lowerNav.classList.add("open");
+    
+};
+
+close_btn.onclick = function () {
+    lowerNav.classList.remove("open");
+}
+
+
+
+
+// function removeLowerNav(){
+//     lowerNav.classList.remove("open")
+// }
+navItems.forEach(item => {
+    item.onclick = () => {
+        lowerNav.classList.remove("open")
+    }
+})
+
+changeLang.onclick = function () {
+
+    if(window.location.href == "http://127.0.0.1:5500/index.html"){
+
+        window.location.href = "http://127.0.0.1:5500/indexrtl.html";
+
+    }else{
+        
+        window.location.href = "http://127.0.0.1:5500/index.html";
+    }
+    
+    
+};
+
+
+
+document.onkeyup = function (e) {
+    if (e.key === "Escape") {
+        lowerNav.classList.remove("open");
+    }
+};
+
+
+
+// Function Change Background When Scroll-Y Reach 150 otherwise reomve background 
+function changeBG() {
+    if (window.scrollY < 100) {
+        nav.classList.remove("nav-backround-animetion")  
+        toggler.style.color = "#eee"
+    } else {
+        nav.classList.add("nav-backround-animetion")
+        toggler.style.color = "#000"
+
+    }
+}
+
+window.addEventListener('scroll', changeBG);
+
+
+
+
+// Function Show Scroll To Up Btutton And Scroll Up When Reach scroll UP 1000
+function scrollToUp(){
+    if(window.scrollY>=500){
+        scrollUp.classList.add("show-scroll-up")
+    }else{
+        scrollUp.classList.remove("show-scroll-up")
+    }
+
+    scrollUp.onclick = () => {
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        })
+    }
+}
+
+window.addEventListener('scroll', scrollToUp);
+
+// if((/iPhone|iPad|iPod|Android|webOS|BlackBerry|Opera Mini|IEMobile/i.test(navigator.userAgent))){
+//     console.log("Phone")
+// }
+
+
